8f0854efcd975604b12ac0612d012aa389c7d013
jayjaisswal <kumarprince13833@gmail.com>
2025-07-11 19:14:06 +0530

    Issue fixing

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,36 @@
-const express = require('express')
-const connectDB = require('./utils/conn')
-const cors = require('cors')
+const express = require("express");
+const connectDB = require("./utils/conn");
+const cors = require("cors");
+require("dotenv").config();
 
-const app = express()
-const port = 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 connectDB();
-app.use(cors({
-  origin: ['https://hosteller-homies-two.vercel.app', 'http://localhost:5173'],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [
+      "https://hosteller-homies-two.vercel.app",
+      "http://localhost:5173",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json({ extended: false }));
 
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/student', require('./routes/studentRoutes'));
-app.use('/api/admin', require('./routes/adminRoutes'));
-app.use('/api/complaint', require('./routes/complaintRoutes'));
-app.use('/api/invoice', require('./routes/invoiceRoutes'));
-app.use('/api/messoff', require('./routes/messoffRoutes'));
-app.use('/api/request', require('./routes/requestRoutes'));
-app.use('/api/attendance', require('./routes/attendanceRoutes'));
-app.use('/api/suggestion', require('./routes/suggestionRoutes'));
-app.use("/api/Event",require('./routes/EventFundRoutes.js'));
-app.use("/api/guard",require('./routes/securityGuards.js'));
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/student", require("./routes/studentRoutes"));
+app.use("/api/admin", require("./routes/adminRoutes"));
+app.use("/api/complaint", require("./routes/complaintRoutes"));
+app.use("/api/invoice", require("./routes/invoiceRoutes"));
+app.use("/api/messoff", require("./routes/messoffRoutes"));
+app.use("/api/request", require("./routes/requestRoutes"));
+app.use("/api/attendance", require("./routes/attendanceRoutes"));
+app.use("/api/suggestion", require("./routes/suggestionRoutes"));
+app.use("/api/Event", require("./routes/EventFundRoutes.js"));
+app.use("/api/guard", require("./routes/securityGuards.js"));
 
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
